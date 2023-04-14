c57d8f8da0746ae13c2cb0f37eb8dfc22f9a3023
ChuchyX <jesuspenax11@gmail.com>
2023-04-13 20:01:41 -0400

    first form completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'msform';
+
+  form: FormGroup;
+  step = 1;
+
+  userData = {
+    name: '',
+    email: '',
+    phone: '',
+  }
+
+  constructor(private formBuilder: FormBuilder)
+  {
+    this.form = formBuilder.group({
+      name: [this.userData.name, Validators.required],
+      email: [this.userData.email, [Validators.required, Validators.email]],
+      phone: [this.userData.phone, [Validators.required]]  
+    });
+  }
+
+  submitForm()
+  {
+    this.step = 2;
+    console.log(this.form.value);
+  }
+
+  goStep2()
+  {
+    this.step = 2;
+  }
+  goStep3()
+  {
+    this.step = 3;
+  }
+  goStep4()
+  {
+    this.step = 4;
+  }
+  goBackStep3()
+  {
+    this.step = 3;
+  }
+  goBackStep2()
+  {
+    this.step = 2;
+  }
+  goBackStep1()
+  {
+    this.step = 1;
+  }
+  ConfirmAll()
+  {
+
+  }
 }
